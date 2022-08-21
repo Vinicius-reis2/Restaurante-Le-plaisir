d254b397be700580b65a4b99bea3ecb6bc4de906
unknown <viniciusdev823@gmail.com>
2022-08-21 13:51:54 -0300

    a

--- js/database.js
+++ js/database.js
@@ -16,35 +16,43 @@ const data = [
         price: 15.00
     },
     {
-        id: 2,
-        image: "https://www.guiadasemana.com.br/contentFiles/image/2018/05/FEA/galeria/57102_w840h525_1525883979shutterstock-187407584.jpg",
-        name: "Croque-Monsieur",
+        id: 3,
+        image: "https://www.guiadasemana.com.br/contentFiles/image/2018/05/FEA/galeria/57102_w840h525_1525883983shutterstock-316198370.jpg",
+        name: "Croque-Madame",
         category: "Aperitivos",
-        description: "O pão é molhado no ovo, coberto com queijo e recheado com presunto e queijo. Depois é só tostar na frigideira.",
+        description: "O Croque Madame é uma versão mais elaborada do Croque-Monsieur. Ele pode ser recheado com saladinhas e ainda ser coberto com um ovo frito bem cremoso.",
+        price: 20.00
+    },
+    {
+        id: 4,
+        image: "https://www.guiadasemana.com.br/contentFiles/image/2018/05/FEA/galeria/57102_w840h525_1525883981shutterstock-214528216.jpg",
+        name: "Crepe",
+        category: "Massas",
+        description: "Essas panquecas de massa fina são típicas da região noroeste do país chamada de Bretagne. Porém você vai encontrar essa delícia em qualquer lugar.",
         price: 15.00
     },
     {
-        id: 2,
-        image: "https://www.guiadasemana.com.br/contentFiles/image/2018/05/FEA/galeria/57102_w840h525_1525883979shutterstock-187407584.jpg",
-        name: "Croque-Monsieur",
-        category: "Aperitivos",
-        description: "O pão é molhado no ovo, coberto com queijo e recheado com presunto e queijo. Depois é só tostar na frigideira.",
-        price: 15.00
+        id: 5,
+        image: "https://www.guiadasemana.com.br/contentFiles/image/2018/05/FEA/galeria/57102_w840h525_1525883982shutterstock-360748985.jpg",
+        name: "Crème Brûlée",
+        category: "Sobremesas",
+        description: "É uma receita preparada com creme de leite, açúcar, ovos e baunilha e finalizada com uma crosta de açúcar queimada.",
+        price: 18.00
     },
     {
-        id: 2,
-        image: "https://www.guiadasemana.com.br/contentFiles/image/2018/05/FEA/galeria/57102_w840h525_1525883979shutterstock-187407584.jpg",
-        name: "Croque-Monsieur",
-        category: "Aperitivos",
-        description: "O pão é molhado no ovo, coberto com queijo e recheado com presunto e queijo. Depois é só tostar na frigideira.",
-        price: 15.00
+        id: 6,
+        image: "https://www.guiadasemana.com.br/contentFiles/image/2018/05/FEA/galeria/57102_w840h525_1525883982shutterstock-360748985.jpg",
+        name: "Macarons",
+        category: "Doces",
+        description: "É uma receita preparada com creme de leite, açúcar, ovos e baunilha e finalizada com uma crosta de açúcar queimada.",
+        price: 18.00
     },
     {
-        id: 2,
-        image: "https://www.guiadasemana.com.br/contentFiles/image/2018/05/FEA/galeria/57102_w840h525_1525883979shutterstock-187407584.jpg",
-        name: "Croque-Monsieur",
-        category: "Aperitivos",
-        description: "O pão é molhado no ovo, coberto com queijo e recheado com presunto e queijo. Depois é só tostar na frigideira.",
-        price: 15.00
+        id: 7,
+        image: "https://www.guiadasemana.com.br/contentFiles/image/2018/05/FEA/galeria/57102_w840h525_1525883987shutterstock-454690144.jpg",
+        name: "Escargot",
+        category: "massas",
+        description: "Você encontra escargots facilmente nos cardápios dos restaurantes franceses. Durante a Idade Média, conta-se que os escargots eram estocados por prevenção para os dias de escassez.",
+        price: 68.00
     }
 ]
